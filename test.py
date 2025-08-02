b207dba0014123fea782d86e0c156b78f1448c9d
bill-BKH <bill.bkh@gmail.com>
2025-08-02 12:26:22 +0330

    adding the timer logic to game

--- test.py
+++ test.py
@@ -4,4 +4,17 @@ import time
 
 x = random.randint(1,6)
 
-print(x)
+print('you are in wild west')
+time.sleep(1)
+print('you see bastardo!!!')
+time.sleep(1)
+print('he slowly reaches to his gun')
+
+time.sleep(random.randint(1,5))
+start = time.time()
+print('DRAW')
+input()
+end  = time.time()
+
+
+print('your speed is '+str(round(end-start)))

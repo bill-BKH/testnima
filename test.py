043d3a92a0d68cc5cb0c26241aa0a54d5150497d
bill-BKH <bill.bkh@gmail.com>
2025-08-02 12:33:08 +0330

    added the logic for speed calculation

--- test.py
+++ test.py
@@ -10,11 +10,17 @@ print('you see bastardo!!!')
 time.sleep(1)
 print('he slowly reaches to his gun')
 
-time.sleep(random.randint(1,5))
+time.sleep(random.randint(1,3))
 start = time.time()
 print('DRAW')
 input()
 end  = time.time()
 
+elapsed_time = round(end-start,2)
+if elapsed_time < 0.5:
+    print('you are the fastest gun drawer in wild west!!!')
+else:
+    print('bastardo kills you!')
 
-print('your speed is '+str(round(end-start)))
+
+print('end')

cce067c3f25c15bd7e68fe478dd8b9c972c34e41
bill-BKH <bill.bkh@gmail.com>
2025-08-02 12:54:13 +0330

    adding health logic to game

--- test.py
+++ test.py
@@ -3,24 +3,26 @@ import time
 import vars
 
 x = random.randint(1,6)
+health = 3
+while health >0: 
+    print('you are in wild west')
+    time.sleep(1)
+    print('you see bastardo!!!')
+    time.sleep(1)
+    print('he slowly reaches to his gun')
 
-print('you are in wild west')
-time.sleep(1)
-print('you see bastardo!!!')
-time.sleep(1)
-print('he slowly reaches to his gun')
+    time.sleep(random.randint(1,3))
+    start = time.time()
+    print('DRAW')
+    input()
+    end  = time.time()
 
-time.sleep(random.randint(1,3))
-start = time.time()
-print('DRAW')
-input()
-end  = time.time()
-
-elapsed_time = round(end-start,2)
-if elapsed_time < vars.minimum_speed:
-    print('you are the fastest gun drawer in wild west!!!')
-else:
-    print('bastardo kills you!')
+    elapsed_time = round(end-start,2)
+    if elapsed_time < vars.minimum_speed:
+        print('you are the fastest gun drawer in wild west!!!')
+    else:
+        print('bastardo kills you!')
+        health -= 1
 
 
 print('end')

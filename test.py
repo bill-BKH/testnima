0d43489a49bf33c5d867a70b3ff0aa863ecd0e4a
nima-pk <pak.nima1383@gmail.com>
2025-08-06 17:24:32 +0330

    code improvment

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 import random
-import time
+from time import sleep,time
 import vars
 from art import bastardo,revolver
 health = 3
 while health >0: 
     print('you are in wild west')
-    time.sleep(1)
+    sleep(1)
     print('you see bastardo!!!')
-    time.sleep(1)
+    sleep(1)
     print('he slowly reaches to his gun')
 
 print('you are in wild west')
-time.sleep(1)
+sleep(1)
 print(bastardo)
 print('you see bastardo!!!')
-time.sleep(1)
+sleep(1)
 print('he slowly reaches to his gun')
 
-time.sleep(random.randint(1,3))
-start = time.time()
+sleep(random.randint(1,3))
+start = time()
 print(revolver)
 print('DRAW')
 input()
-end  = time.time()
+end  = time()
 
 elapsed_time = round(end-start,2)
 if elapsed_time < vars.minimum_speed:
